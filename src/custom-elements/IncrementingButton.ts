cefae09c32406697ef6c52591a8b08b326e78896
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-10 19:51:24 +0200

    refactor(custom-elements): use lit-element

--- src/custom-elements/IncrementingButton.ts
+++ src/custom-elements/IncrementingButton.ts
@@ -1,39 +1,23 @@
-import { ICustomElementDefinition } from "./typing";
+import { html, LitElement } from "lit";
+import { customElement, state } from "lit/decorators.js";
 
-class IncrementingButton extends HTMLElement {
-  button: HTMLButtonElement;
-  count: number = 0;
+@customElement("incrementing-button")
+export class IncrementingButton extends LitElement {
+  @state()
+  count: number;
 
   constructor() {
     super();
-    this.button = document.createElement("button");
-    this.setCount(0);
+    this.count = 0;
   }
 
-  private setCount(value: number) {
-    this.count = value;
-    this.render();
-  }
-
-  private onClick = () => {
-    this.setCount(this.count + 1);
+  private increment = () => {
+    this.count += 1;
   };
 
-  connectedCallback() {
-    this.button.addEventListener("click", this.onClick);
-    this.appendChild(this.button);
-  }
-
-  disconnectedCallback() {
-    this.button.removeEventListener("click", this.onClick);
-  }
-
   render() {
-    this.button.textContent = `Clicked ${this.count} times`;
+    return html`<button @click="${this.increment}">
+      Clicked ${this.count} times
+    </button>`;
   }
 }
-
-export const incrementingButtonDefinition: ICustomElementDefinition = {
-  name: "incrementing-button",
-  constructor: IncrementingButton,
-};
